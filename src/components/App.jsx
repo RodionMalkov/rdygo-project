34dc42eaed0e2bf5fae4b62c8390a74cc322fd86
RodionMalkov <rodionmalkov@gmail.com>
2023-08-01 23:26:00 +0500

    download

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Switch, Route, BrowserRouter} from 'react-router-dom';
+
+import MainPage from './pages/main-page';
+
+const App = () => {
+  return (
+    <MainPage />
+  );
+};
+
+export default App;
---
+++ src/components/pages/main-page.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import {Switch, Route, BrowserRouter} from 'react-router-dom';
+
+import CallbackForm from '../elements/callback-form'
+
+
+const MainPage = (props) => {
+  return (
+    <CallbackForm />
+  );
+};
+
+export default MainPage;
